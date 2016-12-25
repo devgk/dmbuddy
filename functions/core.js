8e8b1a4ff8f4dff2c0853a39ef8fb23e346d0af6
devgk <developergkindia@gmail.com>
2016-12-25 22:15:23 +0530

    0.0.2

--- functions/core.js
+++ functions/core.js
@@ -3,6 +3,7 @@ window.onload = function(){startScript();};
 function startScript(){
 document.getElementById('loadingScreen').style.opacity = '0';
 setTimeout(removeLoading, 150);
+setTimeout(accordanceCode, 50);
 }
 
 function removeLoading(){
@@ -22,4 +23,16 @@ function closeMenu(){
     document.getElementById('headerLeft').style.marginLeft = '-220px';
     document.getElementById('opened').style.display = 'none';
     document.getElementById('closed').style.display = 'block';
+}
+
+function accordanceCode(){
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+     this.classList.toggle("active");
+     this.nextElementSibling.classList.toggle("show");
+  }
+}
 }
