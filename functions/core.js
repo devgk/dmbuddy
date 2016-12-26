504779d825598101151c7f205208bd69e1714454
devgk <developergkindia@gmail.com>
2016-12-26 17:36:07 +0530

    0.0.3

--- functions/core.js
+++ functions/core.js
@@ -14,9 +14,9 @@ elem.parentNode.removeChild(elem);
 function openMenu(){
 document.getElementById('bodyBase').style.marginLeft = '220px';
 document.getElementById('bodyBase').style.marginRigth = '-220px';
-    document.getElementById('headerLeft').style.marginLeft = '0px';
-    document.getElementById('closed').style.display = 'none';
-    document.getElementById('opened').style.display = 'block';
+document.getElementById('headerLeft').style.marginLeft = '0px';
+document.getElementById('closed').style.display = 'none';
+document.getElementById('opened').style.display = 'block';
 }
 
 function closeMenu(){
